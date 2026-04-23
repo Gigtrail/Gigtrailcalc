0d8743d96eb047c80c3bcf790483687219ad5769
Gigtrail <jjbluesgigs@gmail.com>
2026-04-23 14:14:36 +1000

    Fixed lib/api-zod/src/index.ts (line 1).

--- lib/api-zod/src/index.ts
+++ lib/api-zod/src/index.ts
@@ -1,2 +1,28 @@
 export * from "./generated/api";
-export * from "./generated/types";
+
+export type { CreateVenueBody } from "./generated/types/createVenueBody";
+export type {
+  DashboardActualPerformance,
+  DashboardFuturePotential,
+  DashboardSummary,
+} from "./generated/types/dashboardSummary";
+export type {
+  DashboardRecent,
+  DashboardUpcomingTour,
+} from "./generated/types/dashboardRecent";
+export type { ErrorResponse } from "./generated/types/errorResponse";
+export type { HealthStatus } from "./generated/types/healthStatus";
+export type { Profile } from "./generated/types/profile";
+export type { Run } from "./generated/types/run";
+export type { SearchVenuesParams } from "./generated/types/searchVenuesParams";
+export type { Tour } from "./generated/types/tour";
+export type {
+  TourItem,
+  TourItemStatus,
+  TourItemType,
+} from "./generated/types/tourItem";
+export type { TourStop } from "./generated/types/tourStop";
+export type { TourWithStops } from "./generated/types/tourWithStops";
+export type { Vehicle } from "./generated/types/vehicle";
+export type { Venue } from "./generated/types/venue";
+export type { VenueMapItem } from "./generated/types/venueMapItem";
